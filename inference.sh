0ba8b78afae60764186589bc94acb5fa29a8ad4a
woshihg <2766133045@qq.com>
2025-11-03 19:40:36 +0800

    配置文件

---
+++ inference.sh
@@ -0,0 +1,5 @@
+python src/inference_difix.py \
+    --input_image "/home/woshihg/PycharmProjects/Difix3D/dataset/TEST/images" \
+    --prompt "remove degradation" \
+    --output_dir "outputs/difix" \
+    --timestep 199
